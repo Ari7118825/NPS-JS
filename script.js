7b1d360939676beb7649cae67cb8fd86443b5e00
Ari7118825 <159399679+Ari7118825@users.noreply.github.com>
2025-02-01 15:48:32 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+//the following javascript code is able to be executed in a browser and return the user token as a popup on screen
+
+javascript:(function() {
+  const originalFetch = window.fetch;
+  window.fetch = async function(...args) {
+    const response = await originalFetch.apply(this, args);
+    try {
+      const requestBody = args[1]?.body ? JSON.parse(args[1].body) : {};
+      if (requestBody.token) {
+        alert("[Simulation] Token detected in fetch request: " + requestBody.token);
+      }
+    } catch (e) {}
+    return response;
+  };
+
+  const originalXHROpen = XMLHttpRequest.prototype.open;
+  const originalXHRSend = XMLHttpRequest.prototype.send;
+  XMLHttpRequest.prototype.send = function(body) {
+    this.addEventListener('load', function() {
+      try {
+        const requestBody = JSON.parse(body);
+        if (requestBody.token) {
+          alert("[Simulation] Token detected in XHR request: " + requestBody.token);
+        }
+      } catch (e) {}
+    });
+    return originalXHRSend.apply(this, [body]);
+  };
+  console.log("Network monitoring enabled. Tokens will trigger alerts.");
+})();
